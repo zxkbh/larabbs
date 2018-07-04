9effa21af62a4b4532097e7aac4c903a8ad8e6c1
zxkyb <648204712@qq.com>
2018-07-04 15:10:55 +0000

    User can create reply

--- routes/web.php
+++ routes/web.php
@@ -56,4 +56,5 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //话题新建与编辑上传图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+//Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('replies', 'RepliesController', ['only' => ['store','destroy']]);
