2b34b86f94640d20502811ab706985215306fb64
zxkyb <648204712@qq.com>
2018-07-04 16:46:50 +0000

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,24 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Auth;
+
 class User extends Authenticatable
 {
-    use Notifiable;
+    //use Notifiable;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -42,5 +57,13 @@ class User extends Authenticatable
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    //读取消息 未读消息数为0
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Reply;
 
+use App\Notifications\TopicReplied;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -21,6 +22,11 @@ class ReplyObserver
 
     public function created(Reply $reply)
     {
-        $reply->topic->increment('reply_count', 1);
+    	$topic = $reply->topic;
+        //$reply->topic->increment('reply_count', 1);
+        $topic->increment('reply_count', 1);
+    
+    	// 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
     }
 }
