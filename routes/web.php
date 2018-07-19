013294eb2178b366f62294e392ea6614cd3c8a42
zxkyb <648204712@qq.com>
2018-07-19 15:09:23 +0000

    User default avatar after signup

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@
     return view('welcome');
 });*/
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 //Auth::routes();//等同与以下的路由
 
