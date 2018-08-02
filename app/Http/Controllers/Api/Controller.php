bc62349fd88f3e39a699ae7d1a0889d96dfb8676
zxkyb <648204712@qq.com>
2018-08-02 14:21:26 +0000

    verificationCode store

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+// use App\Http\Controllers\Controller;
+use Dingo\Api\Routing\Helpers;
+use App\Http\Controllers\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use Helpers;
+
+    
+}
