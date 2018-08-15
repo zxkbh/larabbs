dfd1d5e75ca1e73ebdc0790599a38b6cd768a2b5
zxkyb <648204712@qq.com>
2018-08-15 15:19:10 +0000

    notifications read

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -23,4 +23,12 @@ class NotificationsController extends Controller
 	        'unread_count' => $this->user()->notification_count,
 	    ]);
 	}
+
+	//标记消息通知为已读
+	public function read()
+	{
+	    $this->user()->markAsRead();
+
+	    return $this->response->noContent();
+	}
 }
